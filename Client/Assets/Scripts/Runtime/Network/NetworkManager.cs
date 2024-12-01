bbb389f86cc7a019d9b02d3a25f531512313de8d
202313175 <202313175@chungkang.academy>
2024-12-01 18:47:20 +0900

    [ADD] Update Server

---
+++ Client/Assets/Scripts/Runtime/Network/NetworkManager.cs
@@ -0,0 +1,152 @@
+using Runtime.BT.Singleton;
+using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Runtime.ConstrainedExecution;
+using TMPro;
+using UnityEngine;
+/// <summary>
+/// 클라이언트 매니저
+/// </summary>
+public class NetworkManager : MonoSingleton<NetworkManager>
+{
+    Queue<byte[]> packetQueue = new Queue<byte[]>();
+
+    TcpClient client;
+    NetworkStream stream;
+
+    [SerializeField]
+    private string server_ip = "127.0.0.1";
+    [SerializeField]
+    private int server_port = 4826;
+    [SerializeField]
+    private static int dataBufferSize = 4096;
+
+    private byte[] receiveBuffer = new byte[dataBufferSize];
+
+
+
+    protected override void Awake()
+    {
+        base.Awake();
+        PacketHandlerPoolManager.Init();
+    }
+    private void Update()
+    {
+        while (packetQueue.Count > 0)
+        {
+            var data = packetQueue.Dequeue();
+            EHandleType type = (EHandleType)data[0];
+            var list = new List<byte>(data);
+            list.RemoveAt(0);
+            data = list.ToArray();
+
+            var handle = PacketHandlerPoolManager.GetPacketHandler(type);
+            handle.Init(data, m_id);
+            handle.OnProcess();
+        }
+    }
+
+    public void Start()
+    {
+
+    }
+    public bool TryConnect()
+    {
+        client = new TcpClient();
+        try
+        {
+            client.Connect("127.0.0.1", 4826);
+        }
+        catch (Exception ex)
+        {
+            Debug.LogError(ex.Message);
+            return false;
+        }
+
+        Debug.Log("접속확인");
+        stream = client.GetStream();
+
+        client.SendBufferSize = dataBufferSize;
+        client.ReceiveBufferSize = dataBufferSize;
+
+        stream.BeginRead(receiveBuffer, 0, dataBufferSize, ReceiveCallback, null);
+
+        return true;
+    }
+    private void ClientConnectCallBack(IAsyncResult result)
+    {
+        client.EndConnect(result);
+        Debug.Log("접속확인");
+        stream = client.GetStream();
+
+        client.SendBufferSize = dataBufferSize;
+        client.ReceiveBufferSize = dataBufferSize;
+    }
+    bool packetRead = false;
+    // 하나의 패킷 스트림
+
+    List<byte> packetStream = new List<byte>();
+    List<byte> packetHeader = new List<byte>();
+    // 패킷완성에 필요한 크기
+    short size;
+    private void ReceiveCallback(IAsyncResult result)
+    {
+        try
+        {
+            int byteLength = stream.EndRead(result);
+
+            if (byteLength <= 0)
+            {
+                // TODO: disconnect
+                return;
+            }
+            byte[] data = new byte[byteLength];
+            Array.Copy(receiveBuffer, data, byteLength);
+            var list = new List<byte>(data);
+
+            // 모든 데이터를 쓸때까지 반복
+            while (list.Count > 0)
+            {
+                // 헤더가 지정되지 않은 상태라면
+                if (packetHeader.Count < 2)
+                {
+                    packetHeader.Add(list[0]);
+                    list.RemoveAt(0);
+                }
+                else
+                {
+                    // 헤더가 완성된 상태라면 사이즈를 구함.
+                    size = BitConverter.ToInt16(packetHeader.ToArray(), 0);
+
+                    // 사이즈와 같아질때까지 반복
+                    if (packetStream.Count < size)
+                    {
+                        packetStream.Add(list[0]);
+                        list.RemoveAt(0);
+
+                        if (packetStream.Count == size)
+                        {
+                            // 같아지거나 더커졋다면 결과물을 인큐 하고 다시반복
+                            packetQueue.Enqueue(packetStream.ToArray());
+                            packetStream.Clear();
+                            packetHeader.Clear();
+                        }
+                    }
+
+                }
+            }
+
+            stream.BeginRead(receiveBuffer, 0, dataBufferSize, ReceiveCallback, null);
+        }
+        catch (Exception _ex)
+        {
+            Debug.Log($"Error receiving TCP data: {_ex}");
+        }
+    }
+    public void SendPacket(PacketHandler packet)
+    {
+        var sendData = packet.MergeData();
+        stream.Write(sendData, 0, sendData.Length);
+    }
+}
---
+++ Client/Assets/Scripts/Runtime/Network/PacketHandlerPoolManager.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public static class PacketHandlerPoolManager
+{
+    static bool m_isInit;
+
+    static Dictionary<EHandleType, List<PacketHandler?>> m_packetHandlerPool =
+        new Dictionary<EHandleType, List<PacketHandler?>>();
+
+    public static void Init()
+    {
+        // 어셈블리안의 핸들러 타입을 전부 수색
+        var assembly = Assembly.GetExecutingAssembly();
+        var types = assembly.GetTypes().Where(_ => _.IsSubclassOf(typeof(PacketHandler)));
+
+
+        // 풀링시킬 객체생성 
+        // - 초기화 과정 또는 풀이 부족할때만 Activate.CreateInstance 를 호출하기 떄문에
+        // - 풀만 잘 조절한다면 괜찮다고 생각함
+        foreach (var type in types)
+        {
+            if (type.IsGenericType) continue;
+
+            string packetTypeStr = type.Name.Substring(0, type.Name.Length - 7);
+
+            EHandleType packetType = Enum.Parse<EHandleType>(packetTypeStr);
+
+            m_packetHandlerPool.Add(
+                packetType,
+                new List<PacketHandler?>());
+
+
+            m_packetHandlerPool[packetType].Add((PacketHandler?)Activator.CreateInstance(type, null, EHandleType.Default));
+
+            Console.WriteLine(packetType);
+        }
+
+
+        m_isInit = true;
+    }
+
+    public static PacketHandler GetPacketHandler(EHandleType packetType)
+    {
+        if (!m_isInit)
+            throw new Exception("[Warning] PacketHandlerPoolManager is not initilize (Use Init method)");
+
+        if (!m_packetHandlerPool.ContainsKey(packetType))
+        {
+            throw new Exception("[Warning] Didn't find packetType correspond handler");
+        }
+        var handle = m_packetHandlerPool[packetType].Find(_ => _.usable);
+
+        // 없다면 리플렉션을 활용해서 타입을 찾고 추가 후 반환 (성능이 느림)
+        if (handle == null)
+        {
+            handle = (PacketHandler?)Activator.CreateInstance(
+                m_packetHandlerPool[packetType][0].GetType(),
+                null, packetType);
+
+            m_packetHandlerPool[packetType].Add(handle);
+        }
+        return handle;
+    }
+
+}
